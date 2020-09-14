4e17f742f5eb91e8fa630aea333bb93040a2aba2
mandeepkhatry <mandeepkhatry1997@gmail.com>
2020-09-14 16:52:53 +0545

    fixes

--- internal/heartbeat/reqres.go
+++ internal/heartbeat/reqres.go
@@ -2,7 +2,6 @@ package heartbeat
 
 import (
 	"encoding/json"
-	"fmt"
 	"service-watch/internal/client"
 	"service-watch/internal/def"
 	"service-watch/internal/generate"
@@ -20,84 +19,97 @@ func ProcessRequest(appConfig models.AppConfig, config map[string]interface{}) e
 
 	access_token := "Bearer " + loginResponse.Message.(map[string]interface{})["data"].(map[string]interface{})["access_token"].(string)
 
-	fmt.Println(access_token)
+	for root, childEps := range appConfig.Endpoints {
+		if root != "/login" {
+			for _, eachChildEp := range childEps {
+				for childEpName, childEpProp := range eachChildEp {
 
-	for _, childEps := range appConfig.Endpoints {
-		for _, eachChildEp := range childEps {
-			for childEpName, childEpProp := range eachChildEp {
+					for _, eachMethod := range childEpProp.Methods {
 
-				for _, eachMethod := range childEpProp.Methods {
+						for methodName, methodOperations := range eachMethod {
+							if _, present := def.SchemaBasedMethods[methodName]; present {
+								if _, contentPresent := methodOperations.RequestBody.Value.Content["application/json"]; contentPresent {
 
-					for methodName, methodOperations := range eachMethod {
-						if _, present := def.SchemaBasedMethods[methodName]; present {
-							if _, contentPresent := methodOperations.RequestBody.Value.Content["application/json"]; contentPresent {
+									component, subcomponent := utils.FindComponent(methodOperations.RequestBody.Value.Content["application/json"].Schema.Ref)
 
-								component, subcomponent := utils.FindComponent(methodOperations.RequestBody.Value.Content["application/json"].Schema.Ref)
+									if component == "schemas" {
 
-								if component == "schemas" {
-									schema := make(map[string]interface{})
-									schemaBytes, _ := appConfig.Components.Schemas[subcomponent].MarshalJSON()
-									json.Unmarshal(schemaBytes, &schema)
-									dummyData := generate.GenerateObject(schema)
+										var schema map[string]interface{}
 
-									dBuffer := models.DataBuffer{}
+										schemaBytes, _ := appConfig.Components.Schemas[subcomponent].MarshalJSON()
 
-									dBuffer.AssignRequest(dummyData)
+										json.Unmarshal(schemaBytes, &schema)
+										dummyData := generate.GenerateObject(schema)
 
-									//TODOImplment Security Options. Not present now in openapi test config. Also Protocol on Reponse Level has to be confirmed.
+										dBuffer := models.DataBuffer{}
 
-									rootEp, resource, isSpecific := utils.IsSpecificItem(childEpName)
+										dBuffer.AssignRequest(dummyData)
 
-									if isSpecific {
+										//TODOImplment Security Options. Not present now in openapi test config. Also Protocol on Reponse Level has to be confirmed.
 
-										//Check for root response for resource ids.
+										rootEp, resource, isSpecific := utils.IsSpecificItem(childEpName)
 
-										resourceItem := endpointsDataBuffer[rootEp]["POST"].Response["data"].(map[string]interface{})[resource].(string)
+										if isSpecific {
 
-										resourceDisplacedEp := rootEp + "/" + resourceItem
+											//Check for root response for resource ids.
 
-										response, _ := httpClient.ExecuteRequest(methodName, resourceDisplacedEp, dummyData, map[string]string{"Authorization": access_token})
+											resourceItem := endpointsDataBuffer[rootEp]["POST"].Response["data"].(map[string]interface{})[resource].(string)
 
-										dBuffer.AssignResponse(response.Message.(map[string]interface{}))
+											resourceDisplacedEp := rootEp + "/" + resourceItem
 
-										endpointsDataBuffer[childEpName][methodName] = dBuffer
+											response, _ := httpClient.ExecuteRequest(methodName, resourceDisplacedEp, dummyData, map[string]string{"Authorization": access_token})
 
-									} else {
-										response, _ := httpClient.ExecuteRequest(methodName, childEpName, dummyData, map[string]string{"Authorization": access_token})
-										dBuffer.AssignResponse(response.Message.(map[string]interface{}))
+											dBuffer.AssignResponse(response.Message.(map[string]interface{}))
 
-										endpointsDataBuffer[childEpName][methodName] = dBuffer
+											endpointsDataBuffer[childEpName] = make(map[string]models.DataBuffer)
+
+											endpointsDataBuffer[childEpName][methodName] = dBuffer
+
+										} else {
+											response, _ := httpClient.ExecuteRequest(methodName, childEpName, dummyData, map[string]string{"Authorization": access_token})
+
+											dBuffer.AssignResponse(response.Message.(map[string]interface{}))
+
+											endpointsDataBuffer[childEpName] = make(map[string]models.DataBuffer)
+
+											endpointsDataBuffer[childEpName][methodName] = dBuffer
+
+										}
+
 									}
 
 								}
+							} else {
+								//Methods GET/DELETE
 
-							}
-						} else {
-							//Methods GET/DELETE
-							rootEp, resource, isSpecific := utils.IsSpecificItem(childEpName)
-							if isSpecific {
-								dBuffer := models.DataBuffer{}
-								//Check for root response for resource ids.
+								rootEp, resource, isSpecific := utils.IsSpecificItem(childEpName)
+								if isSpecific {
+									dBuffer := models.DataBuffer{}
+									//Check for root response for resource ids.
 
-								resourceItem := endpointsDataBuffer[rootEp]["POST"].Response["data"].(map[string]interface{})[resource].(string)
+									resourceItem := endpointsDataBuffer[rootEp]["POST"].Response["data"].(map[string]interface{})[resource].(string)
 
-								resourceDisplacedEp := rootEp + "/" + resourceItem
+									resourceDisplacedEp := rootEp + "/" + resourceItem
 
-								response, _ := httpClient.ExecuteRequest(methodName, resourceDisplacedEp, nil, map[string]string{"Authorization": access_token})
+									response, _ := httpClient.ExecuteRequest(methodName, resourceDisplacedEp, nil, map[string]string{"Authorization": access_token})
 
-								dBuffer.AssignResponse(response.Message.(map[string]interface{}))
+									dBuffer.AssignResponse(response.Message.(map[string]interface{}))
 
-								endpointsDataBuffer[childEpName][methodName] = dBuffer
+									endpointsDataBuffer[childEpName] = make(map[string]models.DataBuffer)
+									endpointsDataBuffer[childEpName][methodName] = dBuffer
+
+								}
 
 							}
 
 						}
 
 					}
-
 				}
 			}
+
 		}
+
 	}
 
 	return nil
--- internal/models/buffer.go
+++ internal/models/buffer.go
@@ -6,12 +6,14 @@ type DataBuffer struct {
 }
 
 func (d *DataBuffer) AssignRequest(requestData map[string]interface{}) {
+	d.Request = make(map[string]interface{})
 	for k, v := range requestData {
 		d.Request[k] = v
 	}
 }
 
 func (d *DataBuffer) AssignResponse(responseData map[string]interface{}) {
+	d.Response = make(map[string]interface{})
 	for k, v := range responseData {
 		d.Response[k] = v
 	}
